784d0cc02b2a754dba4d181fea058b54270e0df3
macolszewski <olschevski@gmail.com>
2018-07-30 22:54:08 +0200

    My first commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 
 
